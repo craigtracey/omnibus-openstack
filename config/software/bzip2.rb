f42f169a07486c62b73b6cfa2c5ba7bc52d3111c
Craig Tracey <craigtracey@gmail.com>
2014-03-30 16:48:56 -0400

    Update software config for broken/slow mirrors
    Update mysql connector to use cogentco as sohu is slow. Parameterize
    the bzip version.

--- config/software/bzip2.rb
+++ config/software/bzip2.rb
@@ -15,7 +15,7 @@
 #
 name    "bzip2"
 default_version "1.0.6"
-source  :url => "http://downloads.openwrt.org/sources/bzip2-1.0.6.tar.gz",
+source  :url => "http://downloads.openwrt.org/sources/bzip2-#{version}.tar.gz",
   :md5 => "00b516f4704d4a7cb50a1d97e6e8e15b"
 
 relative_path "bzip2-#{version}"
